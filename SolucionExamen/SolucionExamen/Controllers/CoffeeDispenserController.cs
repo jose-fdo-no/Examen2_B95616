cefad2b56e7bbe1fe99983f20a3fc2f6eaca473e
jose-fdo-no <josefernando.nunez@ucr.ac.cr>
2023-12-09 05:29:59 -0600

    Added logic to calculate total order price

--- SolucionExamen/SolucionExamen/Controllers/CoffeeDispenserController.cs
+++ SolucionExamen/SolucionExamen/Controllers/CoffeeDispenserController.cs
@@ -7,14 +7,17 @@ namespace SolucionExamen.Controllers
 {
     public class CoffeeDispenserController : Controller
     {
-        public IActionResult Index()
+        private List<CoffeeModel> coffeeList;
+        private int totalOrderCost;
+
+        public CoffeeDispenserController()
         {
-            var coffeeList = GetListOfCoffees();
-            ViewBag.MainTitle = "Lista de cafés disponibles";
-            return View(coffeeList);
+            // Inicializa la lista de cafés en el constructor
+            coffeeList = GetCoffeeList();
+            totalOrderCost = 0;
         }
 
-        private List<CoffeeModel> GetListOfCoffees()
+        private List<CoffeeModel> GetCoffeeList()
         {
             List<CoffeeModel> coffeeList = new List<CoffeeModel>
             {
@@ -23,15 +26,30 @@ namespace SolucionExamen.Controllers
                 new CoffeeModel { Id = 2, CoffeeQuantity = 10, CoffeeType = "Latte", CoffeePrice = 1150 },
                 new CoffeeModel { Id = 3, CoffeeQuantity = 15, CoffeeType = "Mocachino", CoffeePrice = 1300 }
             };
-
             return coffeeList;
+        }
+
+        private List<MoneyModel> GetMoneyList()
+        {
+            List<MoneyModel> moneyList = new List<MoneyModel>
+            {
+                new MoneyModel { Id = 0, MoneyValue = 500, MachineQuantity = 20 },
+                new MoneyModel { Id = 1, MoneyValue = 100, MachineQuantity = 30 },
+                new MoneyModel { Id = 2, MoneyValue = 50, MachineQuantity = 50 },
+                new MoneyModel { Id = 3, MoneyValue = 25, MachineQuantity = 25 }
+            };
+            return moneyList;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MainTitle = "Lista de cafés disponibles";
+            return View(coffeeList);
         }
 
         [HttpPost]
         public IActionResult Ordenar(Dictionary<string, string> coffeeOrder)
         {
-            var coffeeList = GetListOfCoffees();
-
             foreach (var entry in coffeeOrder)
             {
                 int coffeeId;
@@ -49,6 +67,7 @@ namespace SolucionExamen.Controllers
                         if (cafe != null)
                         {
                             cafe.CoffeeQuantity -= quantity;
+                            totalOrderCost += quantity * cafe.CoffeePrice;
                         }
                     }
                 }
