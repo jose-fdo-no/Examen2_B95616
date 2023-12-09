701a1557cb41aefed9254a89b261b277688aeb5f
jose-fdo-no <josefernando.nunez@ucr.ac.cr>
2023-12-09 04:19:44 -0600

    Corrections in CoffeeModel

--- SolucionExamen/SolucionExamen/Models/CoffeeModel.cs
+++ SolucionExamen/SolucionExamen/Models/CoffeeModel.cs
@@ -3,17 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SolucionExamen.Models
 {
-    public class CoffeeModel : Controller
+    public class CoffeeModel
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "La cantidad es requerida.")]
         public int CoffeeQuantity { get; set; }
         public string CoffeeType { get; set; }
         public int CoffeePrice { get; set; }
-        
-        public IActionResult Index(List<CoffeeModel> cofeeList)
-        {
-            return View(cofeeList);
-        }
     }
 }
