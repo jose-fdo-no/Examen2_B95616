68f3bfcc8a3c6fd5a47d08a751785d76b973498b
jose-fdo-no <josefernando.nunez@ucr.ac.cr>
2023-12-09 04:18:41 -0600

    Added MoneyModel

---
+++ SolucionExamen/SolucionExamen/Models/MoneyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SolucionExamen.Models
+{
+    public class MoneyModel
+    {
+        public int Id { get; set; }
+        public int MoneyValue { get; set; }
+        public int MachineQuantity { get; set; }
+    }
+}
