bb3ffc05aa303e311eeb6ca7bdd90596ec51f3e7
jose-fdo-no <josefernando.nunez@ucr.ac.cr>
2023-12-08 18:19:01 -0600

    Added a simple view of the solution

---
+++ SolucionExamen/SolucionExamen/Controllers/CoffeeDispenserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolucionExamen.Models;
+
+namespace SolucionExamen.Controllers
+{
+    public class CoffeeDispenserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var coffeeList = GetListOfCoffees();
+            ViewBag.MainTitle = "Lista de cafés disponibles";
+            return View(coffeeList);
+        }
+
+        private List<CoffeeModel> GetListOfCoffees()
+        {
+            List<CoffeeModel> coffeeList = new List<CoffeeModel>();
+            coffeeList.Add(new CoffeeModel { Id = 0, CoffeeQuantity = 10, CoffeeType = "Americano", CoffeePrice = 850 });
+            coffeeList.Add(new CoffeeModel { Id = 1, CoffeeQuantity = 8, CoffeeType = "Capuchino", CoffeePrice = 950 });
+            coffeeList.Add(new CoffeeModel { Id = 2, CoffeeQuantity = 10, CoffeeType = "Late", CoffeePrice = 1150 });
+            coffeeList.Add(new CoffeeModel { Id = 3, CoffeeQuantity = 15, CoffeeType = "Mocachino", CoffeePrice = 1300 });
+
+            return coffeeList;
+        }
+    }
+}
---
+++ SolucionExamen/SolucionExamen/Models/CoffeeModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolucionExamen.Models
+{
+    public class CoffeeModel : Controller
+    {
+        public int Id { get; set; } 
+        public int CoffeeQuantity { get; set; }
+        public string CoffeeType { get; set; }
+        public int CoffeePrice { get; set; }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
